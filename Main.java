684ff303a7dbe53be66ab2cffab87e21c7416cd6
yousef <yousefelsise440@gmail.com>
2026-04-14 13:35:46 +0200

    up

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     
 }
-Public class Main {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello yousef World!");
         System.out.println("jooooooooooooooooooooooo");
