7c19db771bb5c91ab1156d14ea2fe802d767b6a9
ayoubElashry <datagamer201@gmail.com>
2026-04-15 13:13:00 +0200

    I deleted an unnecessary message and rearranged the code.

--- scr/ui/MainScreen.java
+++ scr/ui/MainScreen.java
@@ -6,14 +6,14 @@ public class MainScreen extends Screen {
 
     @Override
     public void show() {
-        displayHeader("Zakat Fitr Calculator");
-
         ConsoleHelper.clear();
-
+        
+        displayHeader("Zakat Fitr Calculator");
+        
         System.out.println("===========================================");
         System.out.println("Welcome to the Zakat Fitr Calculator!");
         System.out.println("===========================================");
-        System.out.println("\nPress Enter to Start...");
+       
         ConsoleHelper.pause();
     }
 }
